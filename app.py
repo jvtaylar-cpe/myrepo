1bc5ed12b907b7a180fbb7e23d8e965542fcd773
jvtaylar-cpe <90173488+jvtaylar-cpe@users.noreply.github.com>
2023-05-09 21:13:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ model=load_model()
 st.write("""
 # Alzheimer MRI Detection"""
 )
-file=st.file_uploader("Choose plant photo from computer",type=["jpg","png"])
+file=st.file_uploader("Choose an MRI photo from computer",type=["jpg","png"])
 
 import cv2
 from PIL import Image,ImageOps
@@ -18,7 +18,7 @@ def import_and_predict(image_data,model):
     size=(64,64)
     image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
     img=np.asarray(image)
-    img_reshape=img[np.newaxis,...]
+    img_reshape=img[np.newaxis,.]
     prediction=model.predict(img_reshape)
     return prediction
 if file is None:
