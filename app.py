f47b4dbbf8d06ce851d163123b12755219d9bf5a
jvtaylar-cpe <90173488+jvtaylar-cpe@users.noreply.github.com>
2023-05-09 21:57:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,13 +15,38 @@ import cv2
 from PIL import Image,ImageOps
 import numpy as np
 
-def import_and_predict(image_data,model):
-    size=(128,128)
-    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
-    img=np.asarray(image)
-    img_reshape=img[np.newaxis,...]
-    prediction=model.predict(img_reshape)
+# def import_and_predict(image_data,model):
+#     size=(128,128)
+#     image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
+#     img=np.asarray(image)
+#     img_reshape=img[np.newaxis,...]
+#     prediction=model.predict(img_reshape)
+#     return prediction
+
+import cv2
+from PIL import Image, ImageOps
+import numpy as np
+
+def import_and_predict(image_data, model):
+    size = (128, 128)
+    
+    # Resize the image to the expected input shape of the model
+    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+    img = np.asarray(image)
+    img = cv2.resize(img, (128, 128), interpolation=cv2.INTER_NEAREST)
+    
+    # Convert the image to grayscale if necessary
+    if img.ndim == 3 and img.shape[2] == 3:
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+    # Reshape the image to add a channel dimension
+    img_reshape = img.reshape((1,) + img.shape + (1,))
+
+    # Make predictions using the Keras model
+    prediction = model.predict(img_reshape)
     return prediction
+
+
 if file is None:
     st.text("Please upload an image file")
 else:
